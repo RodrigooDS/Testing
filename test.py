97f0486d39924a747e5d2a9fcf46fd9cd947baed
6onza1o <gonzalo.e.espinoza.o@gmail.com>
2020-01-16 02:16:07 -0300

    unit test a medias

---
+++ test.py
@@ -0,0 +1,15 @@
+from app import app
+import unittest
+
+
+class FlaskTestCase(unittest.TestCase):
+
+    # Verifica si Flask esta funcionando
+    def test_index(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
